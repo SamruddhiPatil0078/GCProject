886117b3bb70082a1fdd1733dc163ea0c24982e3
tejask011 <tejj1488@gmail.com>
2026-03-22 22:13:41 +0530

    Added priority improvements v2

--- backend/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.status(401).json({ error: 'Not authenticated' });
-};
-
-module.exports = { isAuthenticated };
